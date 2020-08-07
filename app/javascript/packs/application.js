c792e087c676dff9372d47d4be37198e667c59a7
Jesse Parsons <savvyjesse@aol.com>
2020-08-07 18:09:03 -0400

    fix error in application.html.erb, fix nesting div issue involving postcontainer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,13 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+var likebutton = document.getElementById("like-button")
+var showcommentform = document.getElementById("show-comment-form")
+var showcomments = document.getElementById("show-comments")
+
+showcomments.addEventListener('click', (e) => {
+  console.log("Hello")
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
