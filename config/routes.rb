256293556248aa06206df65542d7dae9e5c73910
Jesse Parsons <savvyjesse@aol.com>
2020-08-07 14:11:25 -0400

    fix devise routes. added seperate path to avoid confliction with /users path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   resources :comments
-  devise_for :users
-
+  devise_for :users, :path => 'u'
   resources :users do
     resources :posts
   end
-#  resources :posts
-  #root "posts#index"
+  resources :posts
+  root "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
