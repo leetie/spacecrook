28179990a8ae32527021ba1e93c86dcb6a9b570d
Jesse Parsons <savvyjesse@aol.com>
2020-08-07 21:54:46 -0400

    add like feature to posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,14 @@
 class LikesController < ApplicationController
+  before_action :set_post
+
+  def create
+    @post.likes.create(user_id: current_user.id)
+    redirect_to user_posts_path(@post.user_id)
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :users do
     resources :posts
   end
-  resources :posts
+  resources :posts do
+    resources :likes
+  end
   root "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
