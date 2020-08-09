f22bf9c739003fe97f8649430c154a8458984a9e
Jesse Parsons <savvyjesse@aol.com>
2020-08-09 10:46:22 -0400

    finish like/unlike feature for posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,33 @@
 class LikesController < ApplicationController
   before_action :set_post
-
+  before_action :set_like, only: [:destroy]
   def create
-    @post.likes.create(user_id: current_user.id)
+    if already_liked?
+      flash[:notice] = "You already liked that!"
+    else
+      @post.likes.create(user_id: current_user.id)
+    end
+    redirect_to user_posts_path(@post.user_id)
+  end
+
+  def destroy
+    if !already_liked?
+      flash[:notice] = "You haven't liked that yet"
+    else
+      @like.destroy
+    end
     redirect_to user_posts_path(@post.user_id)
   end
 
   private
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id: params[:post_id]).exists?
+  end
+
+  def set_like
+    @like = @post.likes.find(params[:id])
+  end
 
   def set_post
     @post = Post.find(params[:post_id])
