2ef6aa4b5761aebc231ad287ecba60c0486bf250
Jesse Parsons <savvyjesse@aol.com>
2020-08-15 15:36:23 -0400

    configure mailer with env variable for gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,10 +32,21 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_options = {from: ENV['EMAIL_USER']}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "example.com",
+    :authentication => :login,
+    :user_name => ENV['EMAIL_USER'],
+    :password => ENV['EMAIL_PASSWORD']
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,21 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_options = {from: ENV['EMAIL_USER']}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "example.com",
+    :authentication => :login,
+    :user_name => ENV['EMAIL_USER'],
+    :password => ENV['EMAIL_PASSWORD']
+  }
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
