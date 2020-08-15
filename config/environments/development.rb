cb87dd6dfdcb95afe9ee93e473571cdb8cfee569
Jesse Parsons <savvyjesse@aol.com>
2020-08-15 17:15:43 -0400

    change development.rb to not perform mailer deliveries, modify seeds.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_options = {from: ENV['EMAIL_USER']}
   config.action_mailer.default_url_options = {:host => 'localhost:3000'}  
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,10 @@
 require 'faker'
 puts "Seeding..."
-#User.create(name: "Jesse", password: "111111", password_confirmation: "111111", email: "savvyjesse@aol.com")
 
 if Rails.env == "development"
   #create users
   20.times do |i| 
-    User.create(name: Faker::Name.unique.name, email: "#{i+1}@aol.com", password: "111111", password_confirmation: "111111")
+    User.create(name: Faker::Name.unique.name, email: "#{i+1}@aol.com", password: "111111", password_confirmation: "111111", confirmed_at: DateTime.now)
   end
 
   #create posts
@@ -31,14 +30,18 @@ if Rails.env == "development"
 end
 
 if Rails.env == "production"
+  #turn off mailer temporarily
+  Rails.application.configure do 
+    config.action_mailer.perform_deliveries = false
+  end
   puts "SHOULD NOT TRIGGER IN DEVELOPMENT"
   #start from this id+1
   start_from = (User.last.id.to_i + 1)
   start_from_posts = (Post.last.id.to_i + 1)
   #create users starting after last current user id
   20.times do |i|
-    User.create(name: Faker::Name.unique.name, email: "#{start_from}@aol.com", password: "111111", password_confirmation: "111111")
-    user = User.find(start_from)
+    User.create(name: Faker::Name.unique.name, email: "#{start_from}@aol.com", password: "111111", password_confirmation: "111111", confirmed_at: DateTime.now)
+    user = User.find(start_from+i)
     #attach profile_pictures for new random users 
     user.profile_picture.attach(io: File.open("./app/assets/images/#{i+1}.png"), filename: "#{i+1}.png")
 
@@ -51,5 +54,9 @@ if Rails.env == "production"
     last_post = Post.last.id.to_i
     last_user = User.last.id.to_i
     Comment.create(post_id: Faker::Number.within(range: start_from_posts..last_post), user_id: Faker::Number.within(range: start_from..last_user), body: Faker::TvShows::SouthPark.quote)
+  end
+  #turn actionmailer back on
+  Rails.application.configure do 
+    config.action_mailer.perform_deliveries = true
   end
 end
