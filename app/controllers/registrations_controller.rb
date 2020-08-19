b8ab70e0350de4431973a427fc187f5dc47e7455
Jesse Parsons <savvyjesse@aol.com>
2020-08-19 15:41:09 -0400

    change registrations controller to allow user resource to be updated if the request.referer was the profile page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,20 @@
 class RegistrationsController < Devise::RegistrationsController
   def update_resource(resource, params)
-    if resource.provider != nil
+    # puts "*" * 100
+    # puts request.inspect
+    if request.referer.present? && request.referer.include?('profile')
+      resource.update_without_password(params)
+    elsif resource.provider != nil
       params.delete("current_password")
       resource.update_without_password(params)
     else
       resource.update_with_password(params)
     end
+    # if resource.provider != nil
+    #   params.delete("current_password")
+    #   resource.update_without_password(params)
+    # else
+    #   resource.update_with_password(params)
+    # end
   end
 end
