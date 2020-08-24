fb6a8f7ced6cf072a31b285b8568dc266e9c6c3f
Jesse Parsons <savvyjesse@aol.com>
2020-08-24 10:52:44 -0400

    change user model to destroy dependent friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,12 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_one_attached :profile_picture, dependent: :destroy
   has_one_attached :profile_background, dependent: :destroy
-  has_many :sent_requests, class_name: "Request", foreign_key: "sent_by", inverse_of: :sender
-  has_many :incoming_requests, class_name: "Request", foreign_key: "sent_to", inverse_of: :receiver
+  has_many :sent_requests, class_name: "Request", foreign_key: "sent_by", inverse_of: :sender, dependent: :destroy
+  has_many :incoming_requests, class_name: "Request", foreign_key: "sent_to", inverse_of: :receiver, dependent: :destroy
   has_many :friends, class_name: "User", foreign_key: "user_id"
   validate :acceptable_image
   validates :name, presence: true
-  has_many :friendships
+  has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
   after_create :add_first_friend
   
