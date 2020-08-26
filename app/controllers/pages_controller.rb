a70420c08a56e5a7b2e3799a8026e4cb2d7b77de
Jesse Parsons <savvyjesse@aol.com>
2020-08-25 22:53:37 -0400

    add magic to queries in pages_controller to only be able to find friends that arent your current friend, and not if they have a pending request for you

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,8 +8,11 @@ class PagesController < ApplicationController
   def users_index
     @friend_ids = current_user.friends.ids
     @friend_ids << current_user.id
-    @users = User.where.not(id: @friend_ids)
-    #only show users that are not in current_user.friends and dont show current_user
+    @sent_request_ids = current_user.sent_requests.map {|r| r.sent_to }
+    @incoming_request_ids = current_user.incoming_requests.map {|r| r.sent_by}
+    # only show users that are not in current_user.friends, dont show current user, don't show user if they have a pending request with current user, and don't show users that have been sent a request already
+    @users = User.where.not(id: @friend_ids).where.not(id: [@sent_request_ids]).where.not(id: [@incoming_request_ids])
+
 
   end
 
