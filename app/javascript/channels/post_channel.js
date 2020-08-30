d9afb2603573627e72132200cab32164b71ebbf3
Jesse Parsons <savvyjesse@aol.com>
2020-08-30 17:16:57 -0400

    add ids to some html elements, add javascript to post_channel.js for notifications to appear when a friend makes a post

--- app/javascript/channels/post_channel.js
+++ app/javascript/channels/post_channel.js
@@ -15,6 +15,22 @@ consumer.subscriptions.create("PostChannel", {
     var user_ids = data.user_ids
     if (user_ids.includes(Number(current_user_id))) {
       $('#post-container').prepend(data.content);
+      // prepend "new post" div with link to top of page if it does not exist
+      var body = document.getElementsByTagName("body")[0];
+      if (document.getElementById("post-notification") == null) {
+        var notice = document.createElement("div");
+        notice.id = "post-notification";
+        notice.classList.add("bg-primary", "row", "position-fixed", "justify-content-center", "pb-2");
+        notice.style.color = "white"
+        notice.innerHTML = '<strong>New Posts</strong>';
+        body.prepend(notice);
+      }
+      //smooth scroll to top and remove notice
+      $("#post-notification").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        $("#post-notification").remove();
+        return false;
+      });
     }
   }
 });
